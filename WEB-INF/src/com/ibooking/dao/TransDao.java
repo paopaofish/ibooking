27e087c8a8f2bde9a6571ed791124235b6ef7fd1
D-S\Administrator <ls_caofs@163.com>
2017-07-20 14:29:05 +0800

    切换开发环境到eclipse

--- WEB-INF/src/com/ibooking/dao/TransDao.java
+++
@@ -1,9 +0,0 @@
-package com.ibooking.dao;
-
-import java.util.List;
-
-import com.ibooking.po.*;
-
-public interface TransDao {
-	void deleteMenuType(List<Menu> lstOldMenu, MenuType oldMenuType, MenuType newMenuType);
-}
--- WEB-INF/src/com/ibooking/util/WebConstant.java
+++
@@ -1,8 +0,0 @@
-package com.ibooking.util;
-
-public class WebConstant {
-	public static final String LOGIN_USER = "loginUser";
-	public static final String LOGIN_AUTH = "loginAuth";
-	
-	public static final int INVALID_VALUE = -1;
-}
